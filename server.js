1133869c0fe88547771a15cf9c1514ab5950f961
sinclayi <sinclayi@gmail.com>
2024-08-28 11:55:20 +0100

    feat: initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const axios = require("axios");
+const process = require("process")
+require('dotenv').config()
+const port = process.env.PORT || 8085
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ data: "Node js server deployed " });
+});
+
+app.get("/port", (req, res) => {
+  res.json({ port });
+});
+
+app.get("/posts", async (req, res) => {
+  try {
+    const { data: posts } = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    const transformedPosts = [];
+    for (let i = 0; i < posts.length; i++) {
+      const post = posts[i];
+      post.userId = i;
+      transformedPosts.push(post);
+    }
+    return res.json({ message: "Fetched information successfully",data: transformedPosts });
+  } catch (error) {
+    console.error("Error getting posts", error);
+    return res.json({
+      message: "Internal Server Error",
+      data: error,
+    });
+  }
+});
+
+app.listen(port, () => {
+    console.log(`Server running successfully on PORT ${port}`)
+});
